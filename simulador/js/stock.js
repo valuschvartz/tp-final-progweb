6b31f28f9c68e969db7da9819d1967b913238ba7
valuschvartz <65975193+valuschvartz@users.noreply.github.com>
2023-10-25 19:26:28 -0300

    Delete simulador/js directory

--- simulador/js/stock.js
+++
@@ -1,8 +0,0 @@
-let Servicios = [
-    {id: 1, nombre: "Peluquería", precio:3000, cantidad:1, img: 'https://www.totalenergies.es/sites/default/files/2022-01/Luz%20peluquer%C3%ADas.jpg'},
-    {id: 2, nombre: "Manicura", precio:2000, cantidad:1, img: 'https://staticsnews.medsbla.com/news-es/wp-content/uploads/2019/08/09133105/manicura-y-pedicura-e1565609133536.jpg'},
-    {id: 3, nombre: "Pedicura", precio:2000, cantidad:1, img: 'https://media.istockphoto.com/photos/pedicure-nail-filing-close-up-picture-id640018462?k=20&m=640018462&s=612x612&w=0&h=IS6HcBBR9qR2KMqhXEVR_PISAgxNKvsJoU26gdQVa1Y='},
-    {id: 4, nombre: "Depilación", precio:1500, cantidad:1, img: 'https://arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/364NW2QEYVCJJMLRDPY4F64YP4.jpg'},
-    {id: 5, nombre: "Maquillaje", precio:1500, cantidad:1, img: 'https://www.cutypaste.com/wp-content/uploads/2014/12/Professional-Makeup-Artist-at-Cinema-Productions-04-2000x1333.jpg'},
-    {id: 6, nombre: "Spa", precio:2000, cantidad:1, img: 'https://promedicas.mx/toluca/wp-content/uploads/2021/05/spa-holistico-beneficios-salud.jpg'},
-]
